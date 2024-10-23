0cc766eabd805f88e91b96f6e98ef6ef9e2e9a71
ck <145850523+hensway@users.noreply.github.com>
2024-10-23 10:14:21 +0000

    Testing interception 4

--- background.js
+++ background.js
@@ -5,16 +5,17 @@ chrome.input.ime.onFocus.addListener((context) => contextID = context.contextID)
 chrome.input.ime.onBlur.addListener((_context) => contextID = 0);
 
 chrome.input.ime.onKeyEvent.addListener((_engineID, keyData, _requestID) => {
-  // if (keyData.evil) return false;
+  if (keyData.evil) return false;
 
-  // if (keyData.key == "a") {
-  //   (async () => {
-  //     keyData.key = "b";
-  //     await chrome.input.ime.sendKeyEvents({contextID, keyData: [keyData]});
-  //     chrome.input.ime.keyEventHandled();
-  //   })();
-  //   return undefined;
-  // }
+  if (keyData.key == "a") {
+    (async () => {
+      keyData.key = "b";
+      keyData.evil = true;
+      await chrome.input.ime.sendKeyEvents({contextID, keyData: [keyData]});
+      chrome.input.ime.keyEventHandled();
+    })();
+    return undefined;
+  }
 
-  return true;
+  return false;
 });
