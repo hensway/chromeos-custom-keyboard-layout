65b030f274ae96862307a97b11f768568df7a083
ck <145850523+hensway@users.noreply.github.com>
2024-10-23 10:05:05 +0000

    Testing interception 3

--- background.js
+++ background.js
@@ -1,19 +1,17 @@
-let ime = chrome.input.ime;
-
 var contextID; // we'll see what uninitialized contextID can do!!!
 
-ime.onFocus.addListener((context) => contextID = context.contextID);
+chrome.input.ime.onFocus.addListener((context) => contextID = context.contextID);
 
-ime.onBlur.addListener((_) => contextID = 0);
+chrome.input.ime.onBlur.addListener((_context) => contextID = 0);
 
-ime.onKeyEvent.addListener((_, keyData, _) => {
+chrome.input.ime.onKeyEvent.addListener((_engineID, keyData, _requestID) => {
   // if (keyData.evil) return false;
 
   // if (keyData.key == "a") {
   //   (async () => {
   //     keyData.key = "b";
-  //     await ime.sendKeyEvents({contextID, keyData: [keyData]});
-  //     ime.keyEventHandled();
+  //     await chrome.input.ime.sendKeyEvents({contextID, keyData: [keyData]});
+  //     chrome.input.ime.keyEventHandled();
   //   })();
   //   return undefined;
   // }
